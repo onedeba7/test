fd44613f62a041c4b3e176273f0f470f411bb867
onedeba7 <onedeba7@gmail.com>
2025-10-30 02:11:27 +0530

    update

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+group = "io.github.onedeba7"
+
+plugins {
+    alias(libs.plugins.publish)
+    alias(libs.plugins.release)
+}
+
+nexusPublishing {
+    repositories {
+        // see https://central.sonatype.org/publish/publish-portal-ossrh-staging-api/#configuration
+        sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+            val ossrhUsername = providers.environmentVariable("OSSRH_USERNAME")
+            val ossrhPassword = providers.environmentVariable("OSSRH_PASSWORD")
+            if (ossrhUsername.isPresent && ossrhPassword.isPresent) {
+                username.set(ossrhUsername.get())
+                password.set(ossrhPassword.get())
+            }
+        }
+    }
+}
+
+// do not generate extra load on Nexus with new staging repository if signing fails
+val initializeSonatypeStagingRepository by tasks.existing
+subprojects {
+    initializeSonatypeStagingRepository {
+        shouldRunAfter(tasks.withType<Sign>())
+    }
+}
